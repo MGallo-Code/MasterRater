5a7d1c720b7f5963663317d2aece354dd243f12f
MGallo-Code <121553614+MGallo-Code@users.noreply.github.com>
2024-08-21 17:59:08 -0400

    Loads bearer token from .env file

--- api_req.py
+++ api_req.py
@@ -1,7 +1,12 @@
+from dotenv import load_dotenv
+import os
 import requests
 
-url = "https://api.themoviedb.org/3/configuration"
+# Load env variables from .env
+load_dotenv()
+bearer_token = os.getenv("BEARER_TOKEN")
 
+url = "https://api.themoviedb.org/3/configuration"
 headers = {
     "accept": "application/json",
     "Authorization": "Bearer " + bearer_token
